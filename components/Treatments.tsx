8ac3ccacb7f2d56d7b7f2a2bb34616464b2cbb3a
Spadro <ysonj5284@gmail.com>
2026-07-13 17:30:06 +0530

    Nudge Choose Your Ritual heading up on mobile

--- components/Treatments.tsx
+++ components/Treatments.tsx
@@ -120,7 +120,7 @@ export default function Treatments() {
         ref={pinRef}
         className="relative flex h-screen w-full items-center justify-center px-6 md:px-16 lg:px-24"
       >
-        <h2 className="absolute left-1/2 top-28 -translate-x-1/2 whitespace-nowrap font-serif text-3xl italic text-cream underline decoration-rose/60 decoration-2 underline-offset-8 sm:top-16 sm:text-5xl">
+        <h2 className="absolute left-1/2 top-[5.5rem] -translate-x-1/2 whitespace-nowrap font-serif text-3xl italic text-cream underline decoration-rose/60 decoration-2 underline-offset-8 sm:top-16 sm:text-5xl">
           Choose Your Ritual
         </h2>
         <div
